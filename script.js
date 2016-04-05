5535cdad11a096d4a25fffd65f148e4d3ae5781d
Drew Stewart <drew.b.stew@gmail.com>
2016-04-05 00:39:48 -0700

    finally got js working!

--- script.js
+++ script.js
@@ -12,28 +12,9 @@ Pseudo-code fun:
 SELECTORS
 */
 
-var quote = document.querySelector("#quote");
-var quoteInfo = document.querySelector("#quote-info")
-var newQuoteButton = document.querySelector("#new-quote-btn")
-
-/*
-EVENTS
-*/
-
-var quoteNumber = getQuoteNumber
-
-newQuoteButton.addEventListener("click", updateQuote);
-
-function getQuoteNumber() {
-  return Math.floor(Math.random() * quotes.length)
-  // random number between 0 & 1, times the number of quotes, then floored
-}
-
-function updateQuote() {
-  getQuoteNumber();
-  quote.innerHTML = '<i class="fa fa-quote-left"></i>' + quotes[quoteNumber].string + '<i class="fa fa-quote-right"></i>'
-  quoteInfo.innerHTML =  'Season ' + quotes[quoteNumber].season + ', Episode ' + quotes[quoteNumber].episode_no + ': ' + quotes[quoteNumber].episode_name + ' (' + quotes[quoteNumber].airdate + ')';
-}
+var quote = document.getElementById("quote");
+var quoteInfo = document.getElementById("quote-info")
+var newQuoteButton = document.getElementById("new-quote-btn")
 
 /*
 QUOTES
@@ -63,6 +44,26 @@ var quotes = [
     airdate:"1991"
   }
 ];
+
+/*
+EVENTS
+*/
+
+var quoteNumber = getQuoteNumber();
+
+newQuoteButton.addEventListener("click", updateQuote);
+
+function getQuoteNumber() {
+  return Math.floor(Math.random() * quotes.length);
+  // random number between 0 & 1, times the number of quotes, then floored
+}
+
+function updateQuote() {
+  quoteNumber = getQuoteNumber();
+  quote.innerHTML = '<i class="fa fa-quote-left"></i>' + quotes[quoteNumber].string + '<i class="fa fa-quote-right"></i>';
+  quoteInfo.innerHTML =  'Season ' + quotes[quoteNumber].season +
+   ', Episode ' + quotes[quoteNumber].episode_no + ': ' + quotes[quoteNumber].episode_name + ' (' + quotes[quoteNumber].airdate + ')';
+}
 
 /*
 DOCUMENT READY
