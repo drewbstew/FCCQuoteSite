96d1bff0669ef12b217b13466f073200f11b2277
Drew Stewart <drew.b.stew@gmail.com>
2016-04-05 01:50:47 -0700

    added more quotes

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Homer Simpson Quote Generator
 
-Features: 
-* 3 (count them: 3!) quotes to choose from at random
+Features:
+* 12 (count them: 12!) quotes to choose from at random
 * New Quote button to get new quote w/out reloading the page
 * Displays information about season & episode #, episode name, and original year of air
 
@@ -10,3 +10,5 @@ Planned to Come:
 * Way more quotes
 * Social links that actually work
 * Maybe better CSS style for buttons? Jesus, I'm not a miracle worker.
+
+Last Commit: 4-5-2016; Added more quotes.
--- script.js
+++ script.js
@@ -37,11 +37,74 @@ var quotes = [
     airdate:"1998"
   },
   {
-    string:"Hey! Just because I don't care doesn't mean I don't understand!",
+    string:"Hey! Just because I don't <em>care</em> doesn't mean I don't understand!",
     season:"2",
     episode_no:"19",
     episode_name: "Lisa's Substitute",
     airdate:"1991"
+  },
+  {
+    string:"I know I'm not normally a praying man, but if you're up there: please, save me, Superman!",
+    season:"9",
+    episode_no:"24",
+    episode_name:"Lost Our Lisa",
+    airdate:"1998"
+  },
+  {
+    string:"Don't blame <em>me</em> - I voted for Kodos!",
+    season:"8",
+    episode_no:"1",
+    episode_name:"Treehouse of Horror VII",
+    airdate:"1996"
+  },
+  {
+    string:"But I can't be a missionary! I don't even <em>believe</em> in Jebus!",
+    season:"11",
+    episode_no:"15",
+    episode_name:"Missionary: Impossible",
+    airdate:"2000",
+  },
+  {
+    string:"Beer! Now there's a temporary solution.",
+    season:"1",
+    episode_no:"3",
+    episode_name:"Homer's Odyssey",
+    airdate:"1990"
+  },
+  {
+    string:"Lisa, in this house, we obey the laws of thermodynamics!",
+    season:"6",
+    episode_no:"21",
+    episode_name:"The PTA Disbands",
+    airdate:"1995"
+  },
+  {
+    string:"I'm a white male, age 18 to 49. Everyone listens to me -- no matter how dumb my suggestions are!",
+    season:"5",
+    episode_no:"14",
+    episode_name:"Lisa vs. Malibu Stacy",
+    airdate:"1994"
+  },
+  {
+    string:"Oh, well, of course, everything looks bad if you remember it.",
+    season:"8",
+    episode_no:"9",
+    episode_name:"El Viaje Misterioso de Nuestro Jomer",
+    airdate:"1997"
+  },
+  {
+    string:"Hey, he's not happy at all! He lied to us through song! I HATE when people do that!",
+    season:"5",
+    episode_no:"13",
+    episode_name:"Homer and Apu",
+    airdate:"1994"
+  },
+  {
+    string:"But Marge... valets! For once maybe someone will call me \"sir\" without adding \"you're making a scene.\"",
+    season:"7",
+    episode_no:"14",
+    episode_name:"Scenes from the Class Struggle in Springfield",
+    airdate: 1996
   }
 ];
 
