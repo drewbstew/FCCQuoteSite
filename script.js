b7150bb019879ad66996f87d9a5868d8f4a9db7d
Drew Stewart <drew.b.stew@gmail.com>
2016-04-05 00:04:21 -0700

    changed background to mirrored homers, since having a static background sucked for smaller windows & mobile site. also added some javascript elements, which aren't working yet.the work continues. still need to scale homer pics so they work on smaller screen.

---
+++ script.js
@@ -0,0 +1,71 @@
+/*
+Site created by DrewBStew, 2016
+
+Pseudo-code fun:
+
+1) Each quote has its own object
+2) objects have properties of string (the actual quote), season#, episode#, episode_name, airdate
+3) Example: string="Mmm... sacrilicious.", season#=5, episode#=16, episode_name="Homes Loves Flanders", airdate="1994"
+*/
+
+/*
+SELECTORS
+*/
+
+var quote = document.querySelector("#quote");
+var quoteInfo = document.querySelector("#quote-info")
+var newQuoteButton = document.querySelector("#new-quote-btn")
+
+/*
+EVENTS
+*/
+
+var quoteNumber = getQuoteNumber
+
+newQuoteButton.addEventListener("click", updateQuote);
+
+function getQuoteNumber() {
+  return Math.floor(Math.random() * quotes.length)
+  // random number between 0 & 1, times the number of quotes, then floored
+}
+
+function updateQuote() {
+  getQuoteNumber();
+  quote.innerHTML = '<i class="fa fa-quote-left"></i>' + quotes[quoteNumber].string + '<i class="fa fa-quote-right"></i>'
+  quoteInfo.innerHTML =  'Season ' + quotes[quoteNumber].season + ', Episode ' + quotes[quoteNumber].episode_no + ': ' + quotes[quoteNumber].episode_name + ' (' + quotes[quoteNumber].airdate + ')';
+}
+
+/*
+QUOTES
+*/
+
+// Can I keep all this shit in a separate JS doc and link to it?
+var quotes = [
+  {
+    string:"Mmm... sacrilicious.",
+    season:"5",
+    episode_no:"16",
+    episode_name:"Homer Loves Flanders",
+    airdate:"1994"
+  },
+  {
+    string:"Oh, they have the internet on computers now!",
+    season:"9",
+    episode_no:"14",
+    episode_name:"Das Bus",
+    airdate:"1998"
+  },
+  {
+    string:"Hey! Just because I don't care doesn't mean I don't understand!",
+    season:"2",
+    episode_no:"19",
+    episode_name: "Lisa's Substitute",
+    airdate:"1991"
+  }
+];
+
+/*
+DOCUMENT READY
+*/
+
+updateQuote();
