551348a75e853d0be8504594be3a9ee73a019279
Drew Stewart <drew.b.stew@gmail.com>
2016-04-06 12:14:22 -0700

    jquery not working; going to find out why

--- JS/script.js
+++ JS/script.js
@@ -2,42 +2,32 @@
 Site created by DrewBStew, 2016
 */
 
-/*
-SELECTORS
-*/
+$(document).ready(function() {
+  // SELECTORS
+  var quote = $("#quote");
+  var quoteInfo = $("#quote-info");
+  var newQuoteButton = $("#new-quote-btn");
 
-var quote = document.getElementById("quote");
-var quoteInfo = document.getElementById("quote-info");
-var newQuoteButton = document.getElementById("new-quote-btn");
-
-/*
-EVENTS
-*/
-
-var quoteNumber = getQuoteNumber();
-
-newQuoteButton.addEventListener("click", updateQuote);
-
-function getQuoteNumber() {
-  return Math.floor(Math.random() * quotes.length);
-  // random number between 0 & 1, times the number of quotes, then floored
-}
-
-function updateQuote() {
-  newRando = getQuoteNumber();
-  while (newRando == quoteNumber) {
-    newRando = getQuoteNumber();
+  // EVENTS
+  function getQuoteNumber() {
+    return Math.floor(Math.random() * quotes.length);
+    // random number between 0 & 1, times the number of quotes, then floored
   }
-  quoteNumber = newRando;
-  // This gets rid of repeated quotes
-  quote.innerHTML = '<i class="fa fa-quote-left"></i>' + quotes[quoteNumber].string + '<i class="fa fa-quote-right"></i>';
-  quoteInfo.innerHTML =  'Season ' + quotes[quoteNumber].season +
-   ', Episode ' + quotes[quoteNumber].episode_no + ': ' +
-   quotes[quoteNumber].episode_name + ' (' + quotes[quoteNumber].airdate + ')';
-}
 
-/*
-DOCUMENT READY
-*/
+  var quoteNumber = getQuoteNumber();
 
-updateQuote();
+  function updateQuote() {
+    newRando = getQuoteNumber();
+    while (newRando == quoteNumber) {
+      newRando = getQuoteNumber();
+    }
+    quoteNumber = newRando;
+    // This gets rid of repeated quotes
+    quote.html = '<i class="fa fa-quote-left"></i>' + quotes[quoteNumber].string + '<i class="fa fa-quote-right"></i>';
+    quoteInfo.text =  'Season ' + quotes[quoteNumber].season +
+     ', Episode ' + quotes[quoteNumber].episode_no + ': ' +
+     quotes[quoteNumber].episode_name + ' (' + quotes[quoteNumber].airdate + ')';
+  }
+  newQuoteButton.click(updateQuote);
+  updateQuote();
+});
